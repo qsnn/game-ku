53efcbb0859b070a4c7f0d1e1998c4fdfd6df78b
千岁奶奶 <ywx2278202141@qq.com>
2024-09-07 15:10:13 +0000

    启用扫雷
    Signed-off-by: 千岁奶奶 <ywx2278202141@qq.com>

---
+++ m、music.c
@@ -0,0 +1,17 @@
+#include<windows.h>
+#include<mmsystem.h>
+#include"resource.h"
+#define WAVE_RESOURCE_ID IDR_WAVE1  
+#define WAVE_RESOURCE_ID IDR_WAVE2
+#pragma comment(lib, "winmm.lib")  
+void music(int musicnum)
+{
+	if (musicnum == 1)
+	{
+		PlaySound(MAKEINTRESOURCE(IDR_WAVE1), NULL, SND_RESOURCE | SND_ASYNC);
+	}
+	else if (musicnum == 2)
+	{
+		PlaySound(MAKEINTRESOURCE(IDR_WAVE2), NULL, SND_RESOURCE | SND_ASYNC);
+	}
+}
